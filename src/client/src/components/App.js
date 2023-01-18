f10a60680a26c3c81e5d4a7422affa42415032ea
kalyaniate789 <kalyaniate789@gmail.com>
2023-01-18 12:54:34 +0530

    add

---
+++ src/client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/App.css';
+import '../styles/bootstrap.min.css'
+
+const App = () => {
+
+  // write your code here
+  return (<>
+    <div>he;llo</div>
+  </>);
+}
+
+
+export default App;
